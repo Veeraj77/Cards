29b15cea4b3a1cdd1a00ec7912c456f7efb00261
Veeraj77 <Veerajsahu07@gmail.com>
2025-02-19 00:47:27 +0530

    creation of main method

--- Main.java
+++ Main.java
@@ -6,3 +6,16 @@ import javax.smartcardio.Card;
 
 class Main{
 
+	public static void main(String args[]){
+		Deck deck = new Deck();
+		System.out.println("________Create Deck________");
+		deck.createDeck();
+		System.out.println("________Print Deck________");
+		deck.displayDeck();
+		System.out.println("########## shuffle Deck #######");
+		deck.shuffleDeck();
+		deck.displayDeck();
+		System.out.println("########## Two Random Card #######");
+		deck.randomCards();
+		deck.displayDeck();
+		}
